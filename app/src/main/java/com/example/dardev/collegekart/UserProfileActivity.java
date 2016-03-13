c46e8e4949075af0329c4386515399925171495b
dev-24 <devashree.mehta95@gmail.com>
2016-03-13 21:43:40 +0530

    tedpicker not functional

--- app/src/main/java/com/example/dardev/collegekart/UserProfileActivity.java
+++ app/src/main/java/com/example/dardev/collegekart/UserProfileActivity.java
@@ -1,14 +1,26 @@
 package com.example.dardev.collegekart;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.gun0912.tedpicker.ImagePickerActivity;
+
+import java.util.ArrayList;
 
 public class UserProfileActivity extends AppCompatActivity {
 
     private Toolbar mToolbar;
+    private static final int INTENT_REQUEST_GET_IMAGES = 1;
+    private ImageView image;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +30,13 @@ public class UserProfileActivity extends AppCompatActivity {
         setSupportActionBar(mToolbar);                   // Setting toolbar as the ActionBar with setSupportActionBar() call
         mToolbar.setTitle("User Profile");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        image= (ImageView) findViewById(R.id.profile_image);
+        image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getImages();
+            }
+        });
     }
 
     @Override
@@ -36,4 +55,22 @@ public class UserProfileActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+    private void getImages() {
+
+        Intent intent  = new Intent(this, ImagePickerActivity.class);
+        startActivityForResult(intent,INTENT_REQUEST_GET_IMAGES);
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resuleCode, Intent intent) {
+        super.onActivityResult(requestCode, resuleCode, intent);
+
+        if (requestCode == INTENT_REQUEST_GET_IMAGES && resuleCode == Activity.RESULT_OK ) {
+
+            ArrayList<Uri> image_uris = intent.getParcelableArrayListExtra(ImagePickerActivity.EXTRA_IMAGE_URIS);
+
+            //do something
+        }
+    }
+
 }
